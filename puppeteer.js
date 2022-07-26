a626b3ed9e2766306784a8570bbd86e247364f67
Randy Truong <randtru@gmail.com>
2022-07-26 01:00:46 -0700

    Added sources

--- puppeteer.js
+++ puppeteer.js
@@ -1,4 +1,10 @@
-// This function can be used for getting HTML of dynamic SPAs after they fully finish loading.
+/* 
+This function can be used for getting HTML of dynamic SPAs after they fully finish loading.
+
+Sources: 
+https://stackoverflow.com/questions/68005766/node-fetch-not-providing-all-html-from-a-page-in-body
+https://stackoverflow.com/questions/54563410/how-to-get-all-html-data-after-all-scripts-and-page-loading-is-done-puppeteer/60617981#60617981
+*/
 (async function main() {
   try {
     const browser = await puppeteer.launch();
